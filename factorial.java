2ff2aed0fe27b8823b137522cfbd26295dea7b3d
mruiz <mruizvillarrazo@uma.es>
2023-02-02 16:30:11 +0100

    implementacion inicial

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+    public class factorial { 
+            public static void main(String[] args) 
+                Scanner scanInput = new Scanner(Sys
+                System.out.print("Introd
+                long 
+                lon g  fac  =  1;
+                    for (l
+         
+                }      
+                System.out
+            scanInput.close();
+    }
+}
